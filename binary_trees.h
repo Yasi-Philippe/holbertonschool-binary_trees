b7a610af40961408c17faa6faf86d808258dfe64
splint314 <krigal323@gmail.com>
2025-12-10 19:01:10 +0100

    fix: compiled the task 17 file

--- binary_trees.h
+++ binary_trees.h
@@ -40,4 +40,6 @@ int binary_tree_balance(const binary_tree_t *tree);
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
+binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 #endif /* BINARY_TREES_H */
