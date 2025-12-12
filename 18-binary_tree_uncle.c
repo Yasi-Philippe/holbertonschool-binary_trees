3168f62012ede6b1d69629651d35a78ef0f13ce2
Yasi-Philippe <shyasi95@gmail.com>
2025-12-12 15:26:19 +0100

    feat: C Function that returns the oncle node in a binary tree

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+
+/**
+ * binary_tree_sibling - finds the sibling of a node
+ * @node: pointer to the node to find the sibling of
+ *
+ * Return:pointer to the sibling node/ NULL if no sibling / node/parent is NULL
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+if (node == NULL || node->parent == NULL)
+return (NULL);
+
+if (node->parent->left == node)
+return (node->parent->right);
+
+if (node->parent->right == node)
+return (node->parent->left);
+
+return (NULL);
+}
+
+
+/**
+ * binary_tree_uncle - Checks the node of the oncle.
+ * @node: Pointer to the nephew node.
+ * Return: A pointer to the oncle node.
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (!node)
+		return (0);
+	if (!node->parent)
+		return (0);
+	return (binary_tree_sibling(node->parent));
+}
