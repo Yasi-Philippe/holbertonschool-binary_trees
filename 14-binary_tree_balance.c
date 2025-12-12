dace3fa6658fbb99136ac1d7658c705c2e9631a0
Yasi-Philippe <shyasi95@gmail.com>
2025-12-12 03:03:17 +0100

    fix: Now properly calculates balance when one child is 0

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -14,7 +14,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t right_height;
 
 	if (!tree)
-		return (-1);
+		return (0);
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
 	return ((left_height > right_height ? left_height : right_height) + 1);
@@ -34,7 +34,7 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 	if (!tree)
 		return (0);
-	height_left = binary_tree_height(tree->left) + 1;
-	height_right = binary_tree_height(tree->right) + 1;
+	height_left = binary_tree_height(tree->left);
+	height_right = binary_tree_height(tree->right);
 	return (height_left - height_right);
 }
